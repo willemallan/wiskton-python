9acf1becfcd408783b664288610106629aba0f46
Willem Allan <willemarf@gmail.com>
2018-02-20 20:00:20 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,15 @@
+# nome utilizamos caracteres ex: nÃºmero e letras
+nome = input("Digite o nome: ")
+# nota usamos float pois possuem . ex: 9.5
+nota1 = float(input("Digite sua nota1: "))
+nota2 = float(input("Digite sua nota2: "))
+media = (nota1+nota2)/2
+idade = 29
+# idade podemos usar apenas nÃºmeros inteiros
+
+if media >= 6:
+	# print("ParabÃ©ns vocÃª passou! Sua nota final foi %s" % media)
+	print("OlÃ¡ {0}! Sua nota final foi {1}! ParabÃ©ns vocÃª passou!".format(nome, media))
+	fwefwe
+else:
+ 	print("Reprovado!")
