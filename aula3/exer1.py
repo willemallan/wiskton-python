02ee2de37f38e0f6e3097a6c3b11c1126e5f4b3e
Willem Allan <willemarf@gmail.com>
2018-03-21 05:01:44 -0300

    aula 3 - for, while
    aula 3 - for, while

---
+++ aula3/exer1.py
@@ -0,0 +1,8 @@
+for x in range(0, 101, 10):
+	print("indice ", x)
+	if x == 150:
+		break
+else:
+	print("else for")
+
+print("terminou")
---
+++ aula3/exer2.py
@@ -0,0 +1,10 @@
+x = 0
+while x < 5:
+	x += 1 # x = x + 1
+	if x == 3:
+		break
+	print("indice ", x)
+else:
+	print("terminou o for - rodou o else")
+
+print("fim")
