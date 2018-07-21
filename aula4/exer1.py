0704f640a338e69778e28187fd609c69c98cbbb3
Willem Allan <willemarf@gmail.com>
2018-07-21 00:06:25 -0300

    Add files via upload
    aula 4

---
+++ aula4/exer1.py
@@ -0,0 +1,8 @@
+def soma(n1, n2):
+	return n1+n2
+
+def resultado(n1, n2):
+	print(n1+n2)
+
+print(soma(5, 5))
+resultado(10, 2)
---
+++ aula4/exer2.py
@@ -0,0 +1,11 @@
+def procedure():
+	print("isto é uma procedure")
+
+def funcao():
+	return "isto é uma função"
+
+procedure()
+print(funcao())
+
+texto = funcao()
+print(texto)
